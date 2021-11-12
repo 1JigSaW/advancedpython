1f57741bcc1397fc6d900a34ef498f3f64b9eb3c
JigSaW <misho_1998@mail.ru>
2021-11-12 19:55:29 +0300

    command line tools

---
+++ advancedpython/sensors_click.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import click
+import socket
+import psutil
+import sys
+
+HELP_TEXT = """порядок вызова: python {program_name:s}
+
+Отображение значения датчиков
+
+Флаги и аргументы:
+
+--help: вывести это сообщение"""
+
+def python_version():
+    return sys.version_info
+
+def ip_addresses():
+    hostname = socket.gethostname()
+
+    adresses = socket.getaddrinfo(socket.gethostname(), None)
+    address_info = []
+    for adress in adresses:
+        address_info.append((adress[0].name, adress[4][0]))
+    return address_info
+
+def cpu_stats():
+    return psutil.cpu_stats()
+
+def cpu_load():
+    return psutil.cpu_percent(interval=0.1)
+
+def cpu_count():
+    return psutil.cpu_count()
+
+def cpu_virtual_memory():
+    return psutil.virtual_memory()
+
+def cpu_users():
+    return psutil.users()
+
+def pids():
+    return psutil.pids()
+
+def swap_memory():
+    return psutil.swap_memory()
+
+def load_avg():
+    return psutil.getloadavg()
+
+def boot_time():
+    psutil.boot_time()
+
+def ram_available():
+    return psutil.virtual_memory().available
+
+def connections():
+    return psutil.net_connections()
+
+@click.command(help='Отображает значения датчиков')
+def show_sensors():
+    print("Версия Python: {0.major}.{0.minor}".format(python_version()))
+    for address in ip_addresses():
+        print("IP-адреса: {0[1]} ({0[0]})".format(address))
+    print("Загрузка ЦП: {:.1f}".format(cpu_load()))
+    print("Доступная память: {} MiB".format(ram_available() / 1024**2))
+
+if __name__ == '__main__':
+    show_sensors()
